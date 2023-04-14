197dd110cb76d4a4b858cf97753c6f36240150c5
jeonghunbak <iiiiilllll@nate.com>
2023-04-14 10:34:05 +0900

    DB INSERT COMMIT

---
+++ src/main/java/com/cloud/boot/web/indexController.java
@@ -0,0 +1,18 @@
+package com.cloud.boot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class indexController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String save(){
+        return "save";
+    }
+}
