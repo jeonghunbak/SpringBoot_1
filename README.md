0dd483f85d24b34f039df2ef1bcb5eb3cc18829f
jeonghunbak <iiiiilllll@nate.com>
2023-04-18 11:28:18 +0900

    SESSION CODE DEDUPLICATION COMMIT

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # SpringBoot_1
 부트연습
+
+JDK 1.8
+Gradle 4.10.2
+Window 10
+
+Google Login
+로그인시 usreName 사용 중 PC 계정 이름 출력
+Window 환경변수명과 동일해 충돌
+userName 말고 다른 변수명으로 변경해 해결
--- src/main/java/com/cloud/boot/web/indexController.java
+++ src/main/java/com/cloud/boot/web/indexController.java
@@ -1,27 +1,25 @@
 package com.cloud.boot.web;
 
+import com.cloud.boot.config.auth.LoginUser;
 import com.cloud.boot.config.auth.dto.SessionUser;
 import com.cloud.boot.service.posts.PostsService;
 import com.cloud.boot.web.dto.PostsResponseDto;
+
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import javax.servlet.http.HttpSession;
-
 @RequiredArgsConstructor
 @Controller
 public class indexController {
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
-
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if(user != null){
             model.addAttribute("name", user.getName()); // userName window에서 사용하는 환경변수 라 PC 유저명 출력됨
