4df9fcbbad28ad4147391a24c83acb2de0aeb395
jeonghunbak <iiiiilllll@nate.com>
2023-04-19 10:09:37 +0900

    NAVER LOGIN COMMIT

--- src/main/java/com/cloud/boot/config/auth/LoginUserArgumentResolver.java
+++ src/main/java/com/cloud/boot/config/auth/LoginUserArgumentResolver.java
@@ -18,7 +18,7 @@ public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver 
 
     @Override
     public boolean supportsParameter(MethodParameter parameter){
-        boolean isLoginAnnotation = parameter.getParameterAnnotation(LoginUser.class) != null;
+        boolean isLoginAnnotation = (parameter.getParameterAnnotation(LoginUser.class)) != null;
         boolean isUserClass = SessionUser.class.equals(parameter.getParameterType());
 
         return isLoginAnnotation && isUserClass;
